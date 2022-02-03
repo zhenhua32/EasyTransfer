097aef0470f748b58494609bdd207343f68d472b
zhenhua32 <zhenhua32@gmail.com>
2022-02-03 10:00:46 +0800

    格式化下代码

--- easytransfer/model_zoo/__init__.py
+++ easytransfer/model_zoo/__init__.py
@@ -18,6 +18,7 @@ import json
 from tensorflow.python.platform import gfile
 from .modeling_adabert import AdaBERTStudent
 
+
 def get_pretrained_model(pretrain_model_name_or_path, **kwargs):
     """
     获取预训练的模型
@@ -27,21 +28,26 @@ def get_pretrained_model(pretrain_model_name_or_path, **kwargs):
         如果 / 不在模型路径中, 说明不是本地文件, 而是一个预先定义的模型名称
         """
         model_type = pretrain_model_name_or_path.split("-")[1]
-        if model_type == 'bert':
+        if model_type == "bert":
             # 重点就看这个模型
             from .modeling_bert import BertPreTrainedModel
+
             return BertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
         elif model_type == "roberta":
             from .modeling_roberta import RobertaPreTrainedModel
+
             return RobertaPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
-        elif model_type == 'albert':
+        elif model_type == "albert":
             from .modeling_albert import AlbertPreTrainedModel
+
             return AlbertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
-        elif model_type == 'imagebert':
+        elif model_type == "imagebert":
             from .modeling_imagebert import ImageBertPreTrainedModel
+
             return ImageBertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
-        elif model_type == 'videobert':
+        elif model_type == "videobert":
             from .modeling_videobert import VideoBertPreTrainedModel
+
             return VideoBertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
         else:
             raise NotImplementedError
@@ -49,7 +55,7 @@ def get_pretrained_model(pretrain_model_name_or_path, **kwargs):
         # 读取模型路径下的 config.json 文件
         config_path = os.path.join(os.path.dirname(pretrain_model_name_or_path), "config.json")
         if "oss" in config_path:
-            with gfile.GFile(config_path, mode='r') as reader:
+            with gfile.GFile(config_path, mode="r") as reader:
                 text = reader.read()
         else:
             with open(config_path, "r") as reader:
@@ -59,20 +65,25 @@ def get_pretrained_model(pretrain_model_name_or_path, **kwargs):
         # 同样的, 也是根据 model_type 来决定如何加载模型
         model_type = json_config["model_type"]
         assert model_type is not None, "you must specify model_type in config.json when pass pretrained_model_path"
-        if model_type == 'bert':
+        if model_type == "bert":
             from .modeling_bert import BertPreTrainedModel
+
             return BertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
         elif model_type == "roberta":
             from .modeling_roberta import RobertaPreTrainedModel
+
             return RobertaPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
-        elif model_type == 'albert':
+        elif model_type == "albert":
             from .modeling_albert import AlbertPreTrainedModel
+
             return AlbertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
-        elif model_type == 'imagebert':
+        elif model_type == "imagebert":
             from .modeling_imagebert import ImageBertPreTrainedModel
+
             return ImageBertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
-        elif model_type == 'videobert':
+        elif model_type == "videobert":
             from .modeling_videobert import VideoBertPreTrainedModel
+
             return VideoBertPreTrainedModel.get(pretrain_model_name_or_path, **kwargs)
         else:
             raise ValueError("model_type should be in bert, roberta, albert, imagebert, videobert")
@@ -82,27 +93,27 @@ def get_config_path(model_type, pretrain_model_name_or_path):
     """
     获取配置路径
     """
-    if model_type == 'bert':
+    if model_type == "bert":
         # 重点就看这个模型, config_path 就是类似于 bert/google-bert-base-zh/config.json
         from .modeling_bert import BertPreTrainedModel
-        config_path = BertPreTrainedModel.pretrained_config_archive_map[
-            pretrain_model_name_or_path]
+
+        config_path = BertPreTrainedModel.pretrained_config_archive_map[pretrain_model_name_or_path]
     elif model_type == "roberta":
         from .modeling_roberta import RobertaPreTrainedModel
-        config_path = RobertaPreTrainedModel.pretrained_config_archive_map[
-            pretrain_model_name_or_path]
-    elif model_type == 'albert':
+
+        config_path = RobertaPreTrainedModel.pretrained_config_archive_map[pretrain_model_name_or_path]
+    elif model_type == "albert":
         from .modeling_albert import AlbertPreTrainedModel
-        config_path = AlbertPreTrainedModel.pretrained_config_archive_map[
-            pretrain_model_name_or_path]
-    elif model_type == 'imagebert':
+
+        config_path = AlbertPreTrainedModel.pretrained_config_archive_map[pretrain_model_name_or_path]
+    elif model_type == "imagebert":
         from .modeling_imagebert import ImageBertPreTrainedModel
-        config_path = ImageBertPreTrainedModel.pretrained_config_archive_map[
-            pretrain_model_name_or_path]
-    elif model_type == 'videobert':
+
+        config_path = ImageBertPreTrainedModel.pretrained_config_archive_map[pretrain_model_name_or_path]
+    elif model_type == "videobert":
         from .modeling_videobert import VideoBertPreTrainedModel
-        config_path = VideoBertPreTrainedModel.pretrained_config_archive_map[
-            pretrain_model_name_or_path]
+
+        config_path = VideoBertPreTrainedModel.pretrained_config_archive_map[pretrain_model_name_or_path]
     else:
         raise ValueError("model_type should be in bert, roberta, albert, imagebert, videobert")
 
